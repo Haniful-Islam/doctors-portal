c11b450d5aafd15b1ca9b7e0c1c06b60b42e3aa7
haniful-islam <haniful360@gmail.com>
2022-05-20 17:00:06 +0600

    navbar added

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is home page</h2>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
